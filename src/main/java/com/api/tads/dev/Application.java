e537f8d749a0050e743dc2646840f8dc3e1ac972
gerlangiosoares <gerlangio@gmail.com>
2023-03-11 19:11:28 -0300

    Projeto criado

---
+++ src/main/java/com/api/tads/dev/Application.java
@@ -0,0 +1,13 @@
+package com.api.tads.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/tads/dev/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.tads.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
